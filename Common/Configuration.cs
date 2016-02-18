b8ffaa78869d5c7303c0e7fe81e3d5b6ef74e871
Kim <KFisher@EKTAU-R90B3062.intra.ektron.com>
2016-02-19 09:02:00 +1100

    Set global properties.

---
+++ Common/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Configuration
+    {
+        public static int LanguageId = int.Parse(ConfigurationSettings.AppSettings["ek_DefaultContentLanguage"]); // 1033;
+
+    }
+}
